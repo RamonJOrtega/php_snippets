89588e38ae5b4d73b60b1866f026a6cf2fc89d87
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-08-13 14:34:01 -0700

    add printing and variables to site.php

--- site.php
+++ site.php
@@ -3,4 +3,13 @@
 //Printing
 echo "<h1>Hello World</h1>";
 
+//Variables
+$name = "Ramon";        //string
+$age = 32;             //integer
+$gpa = 3.83  ;          //decimal
+$isGoodLooking = true;   //booean
+$name = "Ortega";
+echo "Your name is $name <br>";
+
 ?>
+
