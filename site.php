ffcd88840dc3f57d02a0f91228a6ac2580b6b09b
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-08-13 14:43:37 -0700

    add casting and converting to sit.php

--- site.php
+++ site.php
@@ -11,5 +11,10 @@ $isGoodLooking = true;   //booean
 $name = "Ortega";
 echo "Your name is $name <br>";
 
+//Casting and converting
+echo ( (int)3.14 ). "<br>";
+echo ( (int)"80" + (float)"60.5"). "<br>";
+echo (intval("80") + floatval("60.5"));
+
 ?>
 
