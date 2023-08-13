aa27f9d0c5e914c3a450a8a591a2f0c6aefe2e81
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-08-13 14:59:01 -0700

    add basic arithmatic examples for numbers to site.php

--- site.php
+++ site.php
@@ -3,18 +3,29 @@
 //Printing
 echo "<h1>Hello World</h1>";
 
+//numbers
+echo (2*3)."<br>";
+echo(2**3)."<br>"; //2 cubed
+echo(10 % 3)."<br>";
+echo(1 + 2 * 3)."<br>"; //order of operations
+echo(10 / 3.0). "<br>"; //ints and boules turns to doubles
+echo "<br><br>";
+
+
 //Variables
 $name = "Ramon";        //string
 $age = 32;             //integer
 $gpa = 3.83  ;          //decimal
 $isGoodLooking = true;   //booean
 $name = "Ortega";
-echo "Your name is $name <br>";
+echo "Your name is $name <br><br>";
 
 //Casting and converting
 echo ( (int)3.14 ). "<br>";
 echo ( (int)"80" + (float)"60.5"). "<br>";
-echo (intval("80") + floatval("60.5")), "<br>";
+echo (intval("80") + floatval("60.5")), "<br><br>";
+
+
 
 //Strings
 $greeting = "Hello";
@@ -23,7 +34,9 @@ echo strlen($greeting). "<br>";
 echo $greeting[0]."<br>";
 echo $greeting[-1]."<br>";
 echo str_replace("l", "Z",$greeting)."<br>";
-echo strchr($greeting, "ll")."<br>"; //finds the string then rest of string
+echo strchr($greeting, "ll")."<br><br>"; //finds the string then rest of string
+
+
 
 
 
