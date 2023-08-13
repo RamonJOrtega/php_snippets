907d94dba6fb7ce313bf3e89dd69634a37e75bc1
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-08-13 14:52:56 -0700

    add strings and string indexes to site.php

--- site.php
+++ site.php
@@ -14,7 +14,18 @@ echo "Your name is $name <br>";
 //Casting and converting
 echo ( (int)3.14 ). "<br>";
 echo ( (int)"80" + (float)"60.5"). "<br>";
-echo (intval("80") + floatval("60.5"));
+echo (intval("80") + floatval("60.5")), "<br>";
+
+//Strings
+$greeting = "Hello";
+//indexes: 01234
+echo strlen($greeting). "<br>";
+echo $greeting[0]."<br>";
+echo $greeting[-1]."<br>";
+echo str_replace("l", "Z",$greeting)."<br>";
+echo strchr($greeting, "ll")."<br>"; //finds the string then rest of string
+
+
 
 ?>
 
